9fcd497d0fc2fb6cbe1de8ef0c3dd5f8859e515d
Tushar Goyal <tushar.lifetg@gmail.com>
2024-10-23 08:42:03 +0530

    feat: added port in the env

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 setupTwilioWebSocket(wss);
 
